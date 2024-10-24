2dcd5b2239320602918d74e03c37ef92f3e6e63b
HiddenShadow-exe <strumpf.david721@gmail.com>
2024-10-24 13:40:12 +0200

    Unit test for TorpedoStore

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.spaceship;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,61 @@ class TorpedoStoreTest {
 
         // Assert
         assertEquals(true, result);
+    }
+
+    @Test
+    void fire_Fail() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(0);
+
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> store.fire(1));
+    }
+
+    @Test
+    void fire_Fail_TooMany() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1);
+
+        // Assert
+        assertThrows(IllegalArgumentException.class, () -> store.fire(2));
+    }
+
+    @Test
+    void isEmpty_True() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1);
+
+        // Act
+        store.fire(1);
+        boolean result = store.isEmpty();
+
+        // Assert
+        assertEquals(true, result);
+    }
+
+    @Test
+    void failedFire() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(1, 1.0);
+
+        // Act
+        boolean result = store.fire(1);
+
+        // Assert
+        assertEquals(false, result);
+    }
+
+    @Test
+    void checkCount() {
+        // Arrange
+        TorpedoStore store = new TorpedoStore(2);
+
+        // Act
+        store.fire(1);
+        int result = store.getTorpedoCount();
+
+        // Assert
+        assertEquals(1, result);
     }
 }
